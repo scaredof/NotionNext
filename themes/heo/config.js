d846b75f5f2ab0ffecb3d22a3328ef87ddc39c71
scaredof <138845191+scaredof@users.noreply.github.com>
2025-04-08 15:17:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://www.xzy-blog.top' },
-    { title: '访问文档中心获取更多帮助', url: 'https://www.xzy-blog.top' }
+    { title: 'Hi，我是一个程序员, Hi，我是一个干饭人,Hi，我是一个小趴菜', url: 'https://www.xzy-blog.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
